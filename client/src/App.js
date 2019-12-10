308db4a2d8fa0cc7b21456f40cd5f637caa6b2be
Paul Kang <pkang29@gmail.com>
2019-12-10 05:50:27 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Let's go
+      </header>
+    </div>
+  );
+}
+
+export default App;
